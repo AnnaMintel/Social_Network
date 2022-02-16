9f0c415d360931ee789da899698259bc65b12a63
AnnaMintel <annikamintel99@yandex.ru>
2022-02-16 17:17:45 +0300

    feat: creating a components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src="./img/logo.png" alt="" width={100} height={100} />
+    </header>
+  );
+};
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../App.css";
+
+export const Profile = () => {
+    return(
+ <div className="content">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM"
+          alt=""
+        />
+        <div>
+          <img
+            src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+            alt=""
+          />
+        </div>
+        <div>ava + discription</div>
+        <div>
+          {" "}
+          my POsts
+          <div>new posts</div>
+        </div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    
+    );
+}
