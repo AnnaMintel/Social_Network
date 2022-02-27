db677c6b4faa46faead0f452925dd1463a3bf922
AnnaMintel <annikamintel99@yandex.ru>
2022-02-27 16:24:27 +0300

    feat: filled dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import s from "./Dialogs.module.css";
+import s from "./Dialogs.module.css"; 
 
 export const Dialogs = (props: any) => {
     return(
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                   Anna 
+                </div>
+                <div className={s.dialog}>
+                   Angelina 
+                </div>
+                <div className={s.dialog}>
+                   Vanessa
+                </div>
+                <div className={s.dialog}>
+                   Rodrik
+                </div>
+                <div className={s.dialog}>
+                   Ivan 
+                </div>
+                <div className={s.dialog}>
+                   Eva 
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you doing?</div>
+                <div className={s.dialog}>Whats going on?</div>
+            </div>
         </div>
     )
 }
